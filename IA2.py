4c3516f1f8459d6114acb2f3b7b88230b5e6308e
Sam Rundel <samuel.rundel@live.de>
2022-10-22 18:37:28 -0700

    Task 1b,c

--- IA2.py
+++ IA2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # constants
 train_data = "/home/sam/Documents/source/Python/AI534/IA2/IA2-train.csv"
 val = "/home/sam/Documents/source/Python/AI534/IA2/IA2-dev.csv"
-THRESHOLD = 100
+THRESHOLD = 20
 LEARNING_RATE = 0.1
 LAMBDA_VALUE = 1
 
@@ -40,18 +40,49 @@ def regression(x, y, lambda_value):
     m = np.shape(x)[0]  # total number of samples
     n = np.shape(x)[1]  # total number of features
 
-    weights = np.random.randn(n, )
+    weights = np.zeros(n, )
 
     costs = []
 
     while True:
         y_estimated = x.dot(weights)
         error = y_estimated - y
-        ridge_reg_term = (lambda_value / 2 * m) * np.sum(np.square(weights))
+        ridge_reg_term = (lambda_value / m) * np.sum(np.square(weights))
         cost = (0.5 * m) * np.sum(error ** 2) + ridge_reg_term
         gradient = (1 / m) * (x.T.dot(error) + (lambda_value * weights))
         weights = weights - LEARNING_RATE * gradient
-        print(f"iter: {len(costs)}\tcost:{cost}")
+        # print(f"iter: {len(costs)}\tcost:{cost}")
+        costs.append(cost)
+        if len(costs) > 2 and (
+                (costs[len(costs)-2]-costs[len(costs)-1] < THRESHOLD) or costs[len(costs)-1] < 0):
+            return weights, costs
+
+def sig(x):
+    return 1/(1 + np.exp(-x))
+
+def regression2(x, y, lambda_value):
+    m = np.shape(x)[0]  # total number of samples
+    n = np.shape(x)[1]  # total number of features
+
+    weights = np.zeros(n, )
+
+    costs = []
+
+    while True:
+        y_estimated = x.dot(weights)
+        error = y_estimated - y
+        ridge_reg_term = (lambda_value / m) * np.sum(np.square(weights))
+        cost = (0.5 * m) * np.sum(error ** 2) + ridge_reg_term
+        gradient = (1 / m) * (x.T.dot(error) + (lambda_value * weights))
+        for index, weight_value in enumerate(weights):
+            weight_tmp = 0
+            if (abs(weight_value) - (lambda_value *LEARNING_RATE)) < 0:
+                weight_tmp = 0
+            else:
+                weight_tmp = abs(weight_value)-(lambda_value*LEARNING_RATE)
+            weight_tmp*sig(weight_value)
+            weights[index] = weight_tmp
+        # print(f"iter: {len(costs)}\tcost:{cost}")
         costs.append(cost)
         if len(costs) > 2 and (
                 (costs[len(costs)-2]-costs[len(costs)-1] < THRESHOLD) or costs[len(costs)-1] < 0):
@@ -61,13 +92,15 @@ def regression(x, y, lambda_value):
 def check_accuracy(weights, data, classes):
     pred = [0] * len(data)
     dif = 0
-    for index, value in data.iterrows():
-        sum_val = 0
-        for weight_index, weight in enumerate(weights):
-            sum_val += value[weight_index] * weight
-        pred[index] = round(sum_val)
+    estimated = data.dot(weights)
+    for estimated_index, est_value in enumerate(estimated):
+        pred[estimated_index] = round(est_value)
     for index, value in enumerate(pred):
         dif = dif + abs(classes[index] - value)
+
+    dif = (dif/len(data))*100
+    print(dif)
+    print(len(data))
     return dif
 
 
@@ -87,8 +120,16 @@ def lr_l2_train(train_data, classes, val_data, val_classes, lambda_value = LAMBD
 # Trains a logistic regression model with L1 regularization on the provided train_data, using the supplied lambda
 # weights should store the per-feature weights of the learned logisitic regression model. train_acc and val_acc 
 # should store the training and validation accuracy respectively. 
-def LR_L1_train(train_data, val_data, lambda_value):
+def lr_l1_train(train_data, classes, val_data, val_classes, lambda_value = LAMBDA_VALUE):
     # Your code here:
+    weights, cost_history = regression2(train_data, classes.to_numpy(), lambda_value)
+    print(f"Calculation after {len(cost_history)} iterations completed")
+
+    train_acc = check_accuracy(weights, train_data, classes)
+    val_acc = check_accuracy(weights, val_data, val_classes)
+
+    print(train_acc)
+    print(val_acc)
 
     return weights, train_acc, val_acc
 
@@ -113,6 +154,37 @@ def plot_accuracy(lambda_values, train, val):
 def print_weights(weights):
     weights = weights.sort_values(ascending=False)
     print(weights.head(n=5))
+
+
+def calculate2(values, powered=False):
+    data = load_data(train_data)
+    data, classes = preprocess_data(data, True)
+    val_data = load_data(val)
+    val_data, val_classes = preprocess_data(val_data, True)
+
+    train_acc_collected = []
+    val_acc_collected = []
+    weights_collected = []
+    raw_lambda_values = values
+    lambda_values = []
+    for value in raw_lambda_values:
+        if not powered:
+            lambda_values.append(10 ** value)
+        else:
+            lambda_values.append(value)
+
+    for value in lambda_values:
+        LAMBDA_VALUE = value
+        print(f'Training with {LAMBDA_VALUE} as Lambda')
+        weights, train_acc, val_acc = lr_l1_train(data, classes, val_data, val_classes, value)
+        weights_collected.append(weights)
+        train_acc_collected.append(train_acc)
+        val_acc_collected.append(val_acc)
+
+        print_weights(weights)
+
+    plot_accuracy(lambda_values, train_acc_collected, val_acc_collected)
+    return weights_collected
 
 
 def calculate(values, powered=False):
@@ -148,24 +220,27 @@ def calculate(values, powered=False):
 
 
 def Task1a():
-    calculate(range(-4, 3), False)
+    calculate([-4, -3, -2, -1, 0, 0.5, 1, 1.5, 2, 3], False)
 
 def Task1b():
-    calculate([90, 100, 110], True)
+    calculate([10, 100, 1000], True)
 
 def Task1c():
-    weights = calculate(range(-10, 10), False)
+    weights = calculate([0.00000001, 0.0001, 0.001, 0.01, 0.1, 0, 0.5, 0.25, 0.45, 1, 10, 12, 15, 20], True)
     for index_outer, value in enumerate(weights):
         counter = 0
         for index_inner, weight_value in enumerate(value):
             if(abs(weight_value)<(10**-6)):
                 counter = counter + 1
         print(counter)
+        # print(value)
+
+def Task3():
 
 
 
 
-#Task1a()
+# Task1a()
 #Task1b()
 Task1c()
 
